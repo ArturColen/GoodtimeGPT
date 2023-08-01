abb27dee3579f170d8ca9fed12d7c22744881f05
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-07-31 21:05:20 -0300

    Adjustments to page scrolling

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -2,6 +2,7 @@ import { Chat } from "@/types/Chat";
 import { ChatPlaceholder } from "./ChatPlaceholder";
 import { ChatMessageItem } from "./ChatMessageItem";
 import { ChatMessageLoading } from "./ChatMessageLoading";
+import { useEffect, useRef } from "react";
 
 type Props = {
     chat: Chat | undefined;
@@ -9,12 +10,16 @@ type Props = {
 }
 
 export const ChatArea = ({ chat, loading }: Props) => {
+    const scrollArea = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        scrollArea.current?.scrollTo(0, scrollArea.current?.scrollHeight);
+    }, [loading, chat?.messages.length]);
+
     return (
-        <section className="flex-auto h-0 overflow-y-scroll">
+        <section ref={scrollArea} className="flex-auto h-0 overflow-y-scroll">
             {!chat && <ChatPlaceholder />}
-            {chat && chat.messages.map(item => (
-                <ChatMessageItem key={item.id} item={item} />
-            ))}
+            {chat && chat.messages.map(item => (<ChatMessageItem key={item.id} item={item} />))}
             {loading && <ChatMessageLoading />}
         </section>
     );
